c89da58379fcc7388198f4d40d6bf9f37ac528c3
GenayaJames <genayakjames@gmail.com>
2018-04-19 14:33:10 -0400

    initial landing page

---
+++ server.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+const logger = require('morgan');
+
+const port = process.env.port || 3000;
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/tutorial', (req, res) => {
+  res.render('tutorial');
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+})
